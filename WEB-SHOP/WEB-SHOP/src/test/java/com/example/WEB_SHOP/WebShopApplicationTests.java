5589d0b7c625af46d58839d748495efed0cf8e85
TNNQuangg <tnnquangg@gmail.com>
2026-03-08 17:11:03 +0700

    Create

---
+++ WEB-SHOP/WEB-SHOP/src/test/java/com/example/WEB_SHOP/WebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WEB_SHOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
